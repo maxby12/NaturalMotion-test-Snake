ce349c5ff6ec9769ed99f8d4cea9d3930a12cf23
Albert <agarcisa81@alumnes.ub.edu>
2018-07-24 23:01:22 +0200

    Last commit before the delivery of the test implementations

--- NaturalMotion-Test-Snake/Assets/Scripts/GameController.cs
+++ NaturalMotion-Test-Snake/Assets/Scripts/GameController.cs
@@ -9,30 +9,62 @@ public class GameController : MonoBehaviour {
 	public GameObject snake;
 	public float scorePeriod = 5f;
 	public int scoreGain = 10;
+	public float spawnPeriod = 4f;
+	public int maxApples = 4;
+	public Transform applePrefab;
 
 	private float timer;
+	private float spawnTimer;
+	private int ticksToSpawn = 1;
+	private int currentApples;
 	private int score;
 	private Text txtScore; 
+	private GameObject spawnCube;
 
 	// Use this for initialization
 	void Start () {
 		timer = 0;
 		score = 0;
 		txtScore = GameObject.Find("txtScore").GetComponent<Text>();
+		updateScoreUI();
 
-		txtScore.text = score.ToString();
+		currentApples = 0;
+		spawnTimer = 0;
+		spawnCube = GameObject.Find("SpawnCube").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (snake != null) {
 			timer += Time.deltaTime;
+			spawnTimer += Time.deltaTime;
+
 			if (timer > scorePeriod) {
 				score += scoreGain;
 
-				txtScore.text = score.ToString();
+				updateScoreUI();
 
 				timer = 0;
+			}
+
+			if (spawnTimer > spawnPeriod) {
+				if (currentApples >= maxApples) return;
+
+				float w = spawnCube.transform.localScale.x;
+				float h = spawnCube.transform.localScale.y;
+
+				float rand = Random.value;
+				Vector3 randomPosition = spawnCube.transform.position/2 + new Vector3(w * rand, 0, h * rand);
+
+				var hitColliders = Physics.OverlapSphere(randomPosition, 2);
+				if (hitColliders.Length > 1) return; //Only collide with the spawnCube
+
+				Instantiate(applePrefab, spawnCube.transform.position + new Vector3(w * rand, 0, h * rand), applePrefab.transform.rotation);
+				currentApples += 1;
+
+				snake.GetComponent<WormController>().addBodyPart();
+
+				spawnTimer = 0;
 			}
 		}
 		else {
@@ -40,5 +72,18 @@ public class GameController : MonoBehaviour {
 			ApplicationModel.score = score;
 			SceneManager.LoadScene("ScoreDialog");
 		}
+	}
+
+	public void addScore(int score) {
+		this.score += score;
+		updateScoreUI();
+	}
+
+	public void decreaseApples() {
+		currentApples -= 1;
+	}
+
+	private void updateScoreUI() {
+		txtScore.text = score.ToString();
 	}
 }
--- NaturalMotion-Test-Snake/Assets/Scripts/WormMouthController.cs
+++ NaturalMotion-Test-Snake/Assets/Scripts/WormMouthController.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class WormMouthController : MonoBehaviour {
 
+	private bool applePicked;
+	private float timer;
+	private GameController gc;
+
+	void Start() {
+		applePicked = false;
+		timer = 0;
+		gc = GameObject.Find("GameController").GetComponent<GameController>();
+	}
+
+	void FixedUpdate() {
+		if (timer > 1f) {
+			applePicked = false;
+			timer = 0;
+		} else timer += Time.deltaTime;
+	}
+
 	void OnCollisionEnter (Collision col)
 	{
 		print("Mouth: "+col.gameObject.tag);
-		if(col.gameObject.tag == "Worm") {
+
+		if(col.gameObject.tag == "Apple" && !applePicked) {
+			Destroy(col.gameObject);
+			gc.addScore(25);
+			gc.decreaseApples();
+			applePicked = true;
+		}
+		else if(col.gameObject.tag == "Apple Trunk" && !applePicked) {
+			Destroy(col.transform.parent.gameObject);
+			Destroy(col.gameObject);
+			GameObject.Find("GameController").GetComponent<GameController>().addScore(25);
+			applePicked = true;
+		}
+		else if(col.gameObject.tag == "Worm") {
 			Destroy(transform.parent.parent.gameObject);
 		}
 	}
